2fcb7c94c0491f06925f7e5c07869c17cb99f77f
vaibhavsingh22222 <sharmav33@coventry.ac.uk>
2026-04-16 10:35:25 +0000

    commit 5

--- items-app----template/src/App.tsx
+++ items-app----template/src/App.tsx
@@ -8,6 +8,9 @@ import ItemAdmin from "./pages/ItemAdmin";
 import { Amplify } from "aws-amplify"
 import { Authenticator } from "@aws-amplify/ui-react"
 
+/* 🌿 IMPORTANT: THIS FIXES YOUR WHITE / TEXT LOGIN ISSUE */
+import "@aws-amplify/ui-react/styles.css"
+
 Amplify.configure({
   Auth: {
     Cognito:{
@@ -19,39 +22,38 @@ Amplify.configure({
 
 function App() {
   return (
-    <Authenticator.Provider>    
-      <BrowserRouter>
+    <Authenticator.Provider>
 
-        {/* 🌿 Light green global background */}
-        <div className="bg-emerald-50 min-h-screen flex flex-col text-emerald-950">
+      {/* 🌿 AUTH WRAPPER */}
+      <Authenticator>
 
-          {/* 🌿 Navbar */}
-          <div className="bg-emerald-100">
-            <Navbar/>
+        <BrowserRouter>
+
+          <div className="bg-emerald-50 min-h-screen flex flex-col text-emerald-950">
+
+            <div className="bg-emerald-100">
+              <Navbar/>
+            </div>
+
+            <main className="flex-1 px-6 py-6 bg-emerald-50">
+              <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/items" element={<Items/>}/>
+                <Route path="/items/:id" element={<Item/>}/>
+                <Route path="/admin" element={<ItemAdmin/>} />
+              </Routes>
+            </main>
+
+            <div className="bg-emerald-100">
+              <Footer />
+            </div>
+
           </div>
 
-          {/* 🌿 Main content area */}
-          <main className="flex-1 px-6 py-6 bg-emerald-50">
-            <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/items" element={<Items/>}/>
-              <Route path="/items/:id" element={<Item/>}/>
-              <Route path="/admin" element={
-                <Authenticator loginMechanisms={['email']} signUpAttributes={['name']}>
-                  <ItemAdmin/>
-                </Authenticator>
-              } />
-            </Routes>
-          </main>
+        </BrowserRouter>
 
-          {/* 🌿 Footer */}
-          <div className="bg-emerald-100">
-            <Footer />
-          </div>
+      </Authenticator>
 
-        </div>
-
-      </BrowserRouter>
     </Authenticator.Provider>
   )
 }
